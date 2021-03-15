7e048c06d2682063b0fa4707eb6efdac9b946f32
colinkcurtis <colinkcurtis@gmail.com>
2021-03-14 22:09:39 -0400

    idk more changes

--- numerati.py
+++ numerati.py
@@ -10,14 +10,10 @@ class Main:
     def __init__(self):
         self.some_attribute = 'this is an attribute, be default, on the class Main'
 
-
-    def sayHi(self):
-        print('hi')
-
-
-
-
-
+class TrainModel:
+    def __init__(self, modelName, trainingDataFileName):
+        self.modelName = modelName
+        self.trainingDataFileName = trainingDataFileName
 class Predict:
     pass
 
@@ -27,24 +23,17 @@ class Visualize:
 
 if __name__ == "__main__":
 
-
     parser = argparse.ArgumentParser(description="the numereati converge")
 
     parser.add_argument(
-        "-",
-        "--file",
-        help = "inject a file",
-        default = None
-    )
-    parser.add_argument(
         "-m",
-        "--model",
+        "--modelName",
         help = "select the model to train",
         default = None
     )
     parser.add_argument(
         "-t",
-        "--training_data",
+        "--trainingDataFileName",
         help = "select the data set on which to train the model",
         default = None
     )
@@ -57,8 +46,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    newMain = Main()
-    newMain.sayHi()
-    print('data:', args.data)
-    print('model:', args.model)
-    print()
+    trainingDataFileName = args.training_data
+    modelName = args.model
+
+    newModel = TrainModel(modelName, trainingDataFileName)
