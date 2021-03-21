3028fa14511370985859ee0682eeb48245959093
colinkcurtis <colinkcurtis@gmail.com>
2021-03-20 20:30:39 -0400

    moved data-reading to its own file/class

--- numerati.py
+++ numerati.py
@@ -3,10 +3,8 @@ import argparse
 import csv
 from pathlib import Path
 
-import numpy as np
-import pandas as pd
-
 from Model.model_selector import ModelSelector
+from DataHandlers.data_reader import DataReader
 
 MODEL_FILE = Path("Model/trained_models/xgboost1.xgb")
 TARGET_NAME = f"target"
@@ -23,24 +21,9 @@ class Model:
         self.modelName = modelName
         self.trainingDataFileName = trainingDataFileName
 
-    def getData(self):
-        return self.read_csv("Data/example_model_1/numerai_training_data.csv")
-
-    def read_csv(self, file_path):
-        with open(file_path, 'r') as f:
-            column_names = next(csv.reader(f))
-
-        dtypes = {x: np.float16 for x in column_names if x.startswith(('feature', 'target'))}
-        df = pd.read_csv(file_path, dtype=dtypes, index_col=0)
-
-        # Memory constrained? Try this instead (slower, but more memory efficient)
-        # see https://forum.numer.ai/t/saving-memory-with-uint8-features/254
-        # dtypes = {f"target": np.float16}
-        # to_uint8 = lambda x: np.uint8(float(x) * 4)
-        # converters = {x: to_uint8 for x in column_names if x.startswith('feature')}
-        # df = pd.read_csv(file_path, dtype=dtypes, converters=converters)
-
-        return df
+    def readData(self):
+        dataReader = DataReader()
+        return dataReader.read_csv("Data/example_model_1/numerai_training_data.csv")
 
     def train(self):
 
@@ -48,7 +31,7 @@ class Model:
         selectedModel = model.select()
 
 
-        trainingData  = self.getData()
+        trainingData  = self.readData()
         feature_names = [
             f for f in trainingData.columns if f.startswith("feature")
         ]
@@ -61,7 +44,7 @@ class Model:
             print("Training model...")
             selectedModel.fit(trainingData[feature_names], trainingData[TARGET_NAME])
             selectedModel.save_model(MODEL_FILE)
-            
+
 
 
 class Predict:
@@ -101,4 +84,3 @@ if __name__ == "__main__":
     newModel = Model(modelName, trainingDataFileName)
 
     newModel.train()
-
